4fc702692f2c422ef8ff570e9cc8723466d4e484
p13seva <p13seva@ionio.gr>
2018-03-13 04:52:26 +0200

    Update matrix2.c

--- matrix2.c
+++ matrix2.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix2.c -o matrix2
-
-
 #define NCOLS 100
+#define NROWS 100000
 
 void get_walltime(double *wct) {
   struct timeval tp;
@@ -13,40 +11,42 @@ void get_walltime(double *wct) {
   *wct = (double)(tp.tv_sec+tp.tv_usec/1000000.0);
 }
 
-
-int main() {
+int main(int argc, char *argv[]) {
+int i,j;
+double sum = 0.0;
 double *table;
-double ts,te;
+double ts,te, Maccess;
 
-
-  table = (double *)malloc(NROWS*NCOLS*sizeof(double)); 
+  table = (double *)malloc(NCOLS*NROWS*sizeof(double));
   if (table==NULL) {
     printf("alloc error!\n");
     exit(1);
   }
 
-  // warmup
+  
+  for(i=0; i<NCOLS*NROWS; i++){
+  	table[i]=1.0;
+  }
 
-  // ...your code here...
-
-  // get starting time (double, seconds) 
   get_walltime(&ts);
   
-  // workload
+  // προσπελαση κατα στήλη
 
-  // ...your code here...
-
-  // get ending time
+  for (i=0;i< NCOLS;i++){
+  	for(j=0;j< NROWS;j++){
+  		sum+=table[j*NCOLS+i];
+ 	 }
+  }
   get_walltime(&te);
 
-  // check results
-  
-  // ...your code here...
+  printf("Accesses = %f\n", sum);
 
-  // print time elapsed and/or Maccesses/sec
-  
-  // ...your code here...  
-  
+  //To Maccess ειναι τα  Memoty_accesses per second
+
+   Maccess=((double)NROWS*NCOLS)/((te-ts)*1e6);
+
+  printf("Maccesses/sec = %f\n" , Maccess);
+
   free(table);
 
   return 0;
